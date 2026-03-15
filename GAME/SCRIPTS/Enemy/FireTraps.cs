7d9e6102949ee0602b1b064c8d97c7fd6bef9c5d
Lazar Luisa <140090887+luiiluisa@users.noreply.github.com>
2026-03-15 20:21:54 +0200

    Add files via upload

---
+++ GAME/SCRIPTS/Enemy/FireTraps.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireTraps : MonoBehaviour
+{
+    [SerializeField] private float damage = 1f;
+
+    [Header("Firetrap Timers")]
+    [SerializeField] private float activationDelay = 0.5f;
+    [SerializeField] private float activeTime = 2f;
+
+    [Header("Damage Settings")]
+    [SerializeField] private float damageCooldown = 1f;
+
+    private Animator anim;
+    private SpriteRenderer spriteRend;
+
+    private bool triggered;
+    private bool active;
+    private bool playerInside;
+
+    private float damageTimer;
+    private Health playerHealth;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (active && playerInside)
+        {
+            damageTimer -= Time.deltaTime;
+
+            if (damageTimer <= 0)
+            {
+                playerHealth.TakeDamage(damage);
+                damageTimer = damageCooldown;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInside = true;
+            playerHealth = collision.GetComponent<Health>();
+
+            if (!triggered)
+                StartCoroutine(ActivateFiretrap());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerInside = false;
+        }
+    }
+
+    private IEnumerator ActivateFiretrap()
+    {
+        triggered = true;
+
+        spriteRend.color = Color.red;
+
+        yield return new WaitForSeconds(activationDelay);
+
+        spriteRend.color = Color.white;
+        active = true;
+        anim.SetBool("activated", true);
+
+        yield return new WaitForSeconds(activeTime);
+
+        active = false;
+        triggered = false;
+        anim.SetBool("activated", false);
+    }
+}
---
+++ GAME/SCRIPTS/Rooms/CameraController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //Room camera
+    
+    private float currentPosX;
+    
+    //Follow player
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
+
+    private void Update()
+    {
+        
+        //Follow player
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+    }
+
+    public void MoveToNewRoom(Transform _newRoom)
+    {
+        currentPosX = _newRoom.position.x;
+    }
+}
